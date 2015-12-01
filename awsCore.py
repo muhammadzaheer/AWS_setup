81acd07937909c81224f88f02cdbdad77a1ea5df
Muhammad Zaheer <mzaheer@plumgrid.com>
2015-12-01 18:52:25 +0500

    Added awsCore.py and awsCoreRelease.py
    awsCore.py
    -       Sets up core topology on AWS
    -       Requires a string to be used as name tag
    
    awsCoreRelease.py
    -       Releases the core topology
    -       Requires a conf_file which is created by running awsCore.py

---
+++ awsCore.py
@@ -0,0 +1,210 @@
+import boto.ec2;
+import boto.vpc;
+
+import time;
+import json;
+import sys;
+
+class awsCore (object):
+    
+    def __init__ (self, name):
+            """
+            Dictionary 'conf' holds identifiers for VPC resources
+            It would be serialized before the script exits
+            Later, it would be used to add private subnets to the VPC
+            and free-up VPC resources
+            """
+            self.conf = {"name" : name}; 
+            self.ec2_conn = self.__create_ec2_conn(); 
+            self.vpc_conn = self.__create_vpc_conn(); 
+            self.vpc = self.__create_vpc(); 
+            
+            self.public_subnet = self.__create_public_subnet(); 
+            self.ig = self.__create_attach_internet_gateway();
+            self.main_rtable = self.__get_main_rtable();
+            self.custom_rtable, self.custom_rtable_assoc = self.__create_custom_rtable();
+            # TODO: Delete All trafic rules from both security groups
+            self.sg_nat = self.__create_sg_nat();
+            self.sg_private = self.__create_sg_private();
+            self.nat_instance = self.__run_nat_instance();
+            self.eip = self.__allocate_eip_nat();
+            self.__create_route_to_nat();
+            
+            self.__populate_conf(); 
+            self.__persist_conf(); 
+    
+    def __create_ec2_conn (self):
+        singapore = boto.ec2.regions()[4];
+        return boto.ec2.connect_to_region (region_name = singapore.name);
+    
+    def __create_vpc_conn (self):
+        singapore = boto.ec2.regions()[4];
+        return boto.vpc.VPCConnection (region = singapore);
+    
+    def __create_vpc (self):
+        vpc = self.vpc_conn.create_vpc (cidr_block = "10.0.0.0/16");
+        vpc.add_tag("Name", self.conf["name"] + "_VPC");
+        return vpc;
+    
+    def __create_public_subnet (self):
+        public_subnet = self.vpc_conn.create_subnet (self.vpc.id, 
+                                                cidr_block = "10.0.0.0/24");
+        
+        public_subnet.add_tag("Name", self.conf["name"] + "_public_subnet");
+        return public_subnet;
+    
+    def __create_attach_internet_gateway (self):
+        ig = self.vpc_conn.create_internet_gateway();
+        ig.add_tag ("Name", self.conf["name"] + "_ig");
+        self.vpc_conn.attach_internet_gateway(ig.id, self.vpc.id);
+        return ig;
+    
+    def __get_main_rtable(self):
+        return self.vpc_conn.get_all_route_tables(filters = {'association.main':'true', 
+                                                        'vpc-id':self.vpc.id})[0];
+    """
+    This method creates a custom routing table for public subnet
+    And adds a route for egress traffic to internet gateway
+    """
+    def __create_custom_rtable(self):
+        custom_rtable = self.vpc_conn.create_route_table(self.vpc.id);
+        custom_rtable.add_tag ("Name", self.conf["name"] + "_custom_rtable");
+        self.vpc_conn.create_route(custom_rtable.id, "0.0.0.0/0", self.ig.id);
+        custom_rtable_assoc = self.vpc_conn.associate_route_table (custom_rtable.id, 
+								self.public_subnet.id);
+        return custom_rtable, custom_rtable_assoc;
+    
+    def __create_sg_nat(self):
+        sg_nat = self.ec2_conn.create_security_group (name = self.conf["name"] + "_SGNAT",
+                            description = "Security Group for NAT instances",
+                            vpc_id = self.vpc.id);
+        sg_nat.add_tag("Name", self.conf["name"]+ "_SGNAT");
+        self.__add_sgnat_rules(sg_nat);
+        return sg_nat;
+    
+    def __create_sg_private(self):
+        sg_private = self.ec2_conn.create_security_group (
+                                name = self.conf["name"]+ "_Private", 
+                                description= "Security group for private instances",
+                                vpc_id = self.vpc.id);
+        sg_private.add_tag("Name", self.conf["name"] +"_Private");
+        self.__add_sgprivate_rules(sg_private);
+        return sg_private;
+    
+    def __run_nat_instance (self):
+        
+        reservation = self.ec2_conn.run_instances (image_id = 'ami-1a9dac48', min_count = 1, 
+			max_count = 1, key_name ='openstack-workshop', instance_type = 't2.micro', 
+			subnet_id = self.public_subnet.id, private_ip_address='10.0.0.5', 
+                    	security_group_ids = [self.sg_nat.id]);
+    
+        nat_instance = reservation.instances[0];
+        nat_instance.add_tag("Name", self.conf["name"] + "_NAT");
+        # Each EC2 instance performs source/destination checks by default. 
+        # This means that the instance must be the source or destination of any 
+        # traffic it sends or receives. 
+        # However, a NAT instance must be able to send and receive traffic when 
+        # the source or destination is not itself. 
+        # Therefore, we must disable source/destination checks on the NAT instance
+        
+        self.ec2_conn.modify_instance_attribute(instance_id = nat_instance.id, 
+                                           attribute = 'sourceDestCheck', 
+                                           value=False);
+        
+        # Wait for the NAT instance to get into running state 
+        # We cannot assing an EIP to it until it is in the running
+        while True: 
+            reservations = self.ec2_conn.get_all_instances(instance_ids = [nat_instance.id]);
+            if reservations[0].instances[0].state != 'running':
+                print "instance {} starting...".format(reservations[0].instances[0].id);
+                time.sleep(10);
+            else:
+                return nat_instance; 
+    
+    def __allocate_eip_nat (self):
+        # Allocating an elastic IP address for our NAT instance
+        eip = self.ec2_conn.allocate_address(domain="vpc");
+        # Associating EIP with NAT instance
+        eip.association_id = self.ec2_conn.associate_address_object (
+                            instance_id = self.nat_instance.id,
+                            allocation_id = eip.allocation_id).association_id;
+        
+        return eip;
+    
+    def __create_route_to_nat (self):
+        # Creating a route in main route table such that all outgoing traffic
+        # from private subnets will be sent to our NAT instance
+        self.vpc_conn.create_route(self.main_rtable.id, "0.0.0.0/0", 
+                                   instance_id = self.nat_instance.id);
+        
+    def __add_sgnat_rules (self, sg_nat):
+        # inbound traffic rules
+        # HTTP from private subnet
+        self.ec2_conn.authorize_security_group(group_id = sg_nat.id, ip_protocol = "tcp",
+                        from_port = 80, to_port = 80, cidr_ip = "10.0.1.0/24");
+        # HTTPS from private subnet
+        self.ec2_conn.authorize_security_group(group_id = sg_nat.id, ip_protocol = "tcp",
+                        from_port = 443, to_port = 443, cidr_ip = "10.0.1.0/24");
+        # SSH from anywhere
+        self.ec2_conn.authorize_security_group(group_id = sg_nat.id, ip_protocol = "tcp",
+                        from_port = 22, to_port = 22, cidr_ip = "0.0.0.0/0");
+        # ICMP from anywhere
+        self.ec2_conn.authorize_security_group(group_id = sg_nat.id, ip_protocol = "icmp",
+                        from_port = -1, to_port = -1, cidr_ip = "0.0.0.0/0");
+    
+        # outbound traffic rules
+        # HTTP to anywhere
+        self.ec2_conn.authorize_security_group_egress (group_id = sg_nat.id, ip_protocol = "tcp",
+                        from_port = 80, to_port = 80, cidr_ip = "0.0.0.0/0");
+        # HTTPS to anywhere
+        self.ec2_conn.authorize_security_group_egress (group_id = sg_nat.id, ip_protocol = "tcp",
+                        from_port = 443, to_port = 443, cidr_ip = "0.0.0.0/0");
+        # SSH to private subnet
+        self.ec2_conn.authorize_security_group_egress (group_id = sg_nat.id, ip_protocol = "tcp",
+                        from_port = 22, to_port = 22, cidr_ip = "10.0.1.0/24");
+        # ICMP to anywhere
+        self.ec2_conn.authorize_security_group_egress (group_id = sg_nat.id, ip_protocol = "icmp",
+                        from_port = -1, to_port = -1, cidr_ip = "0.0.0.0/0");
+        
+    def __add_sgprivate_rules(self,sg_private):
+        # inbound traffic rules
+        # SSH from public subnet
+        self.ec2_conn.authorize_security_group(group_id = sg_private.id, ip_protocol = "tcp",
+                            from_port = 22, to_port = 22, cidr_ip = "10.0.0.0/24");
+        # outbound traffic rules
+        # ICMP to anywhere
+        self.ec2_conn.authorize_security_group_egress (group_id = sg_private.id, 
+				ip_protocol = "icmp", from_port = -1, 
+				to_port = -1, cidr_ip = "0.0.0.0/0");
+        # HTTP to anywhere
+        self.ec2_conn.authorize_security_group_egress (group_id = sg_private.id, 
+				ip_protocol = "tcp",from_port = 80, 
+				to_port = 80, cidr_ip = "0.0.0.0/0");
+        # HTTPS to anywhere
+        self.ec2_conn.authorize_security_group_egress (group_id = sg_private.id, 
+				ip_protocol = "tcp",from_port = 443, 
+				to_port = 443, cidr_ip = "0.0.0.0/0"); 
+        
+    def __populate_conf(self):
+        self.conf["vpc"] = self.vpc.id;
+        self.conf["public_subnet"] = self.public_subnet.id;
+        self.conf["ig"] = self.ig.id;
+        self.conf["main_rtable"] = self.main_rtable.id;
+        self.conf["custom_rtable"] = self.custom_rtable.id;
+        self.conf["custom_rtable_assoc"] = self.custom_rtable_assoc;
+        self.conf["sg_nat"] = self.sg_nat.id;
+        self.conf["sg_private"] = self.sg_private.id;
+        self.conf["nat_instance"] = self.nat_instance.id;
+        self.conf["eip"] = self.eip.public_ip;
+        self.conf["eip_alloc"] = self.eip.allocation_id;
+        self.conf["eip_assoc"] = self.eip.association_id;
+    
+    def __persist_conf(self):
+        with open ('conf.json','w') as fp:
+            json.dump(self.conf,fp);    
+if __name__ == '__main__':
+    if len (sys.argv) != 2:
+        print 'Usage: awsCore.py <name_tag>';
+        sys.exit(-1);
+    
+    aws = awsCore (sys.argv[1]);
---
+++ awsCoreRelease.py
@@ -0,0 +1,67 @@
+import boto.ec2;
+import boto.vpc;
+
+import time;
+import json; 
+import sys;
+
+class awsCoreRelease (object):
+    
+    def __init__ (self, conf_file):
+        try:
+            with open (conf_file, 'r') as fp:
+                self.conf = json.load(fp);
+        
+            self.ec2_conn = self.__create_ec2_conn();
+            self.vpc_conn = self.__create_vpc_conn();        
+            self.__release();
+        except IOError:
+            print 'IOError: Conf file not found';
+            sys.exit(-1);
+        
+        except KeyError as e:
+            print "Conf.json does not contain key " + e.args[0];
+            print "Make sure Conf file is valid and contains all resource ids";
+            sys.exit(-1);
+
+    def __create_ec2_conn (self):
+        singapore = boto.ec2.regions()[4];
+        return boto.ec2.connect_to_region (region_name = singapore.name);
+
+    def __create_vpc_conn (self):
+        singapore = boto.ec2.regions()[4];
+        return boto.vpc.VPCConnection (region = singapore);
+
+    def __release (self):
+        self.ec2_conn.delete_security_group (group_id = self.conf["sg_private"]);
+        self.ec2_conn.disassociate_address (association_id = self.conf["eip_assoc"]);
+        self.ec2_conn.release_address (allocation_id = self.conf["eip_alloc"]);
+        self.__terminate_nat_instance();
+        self.ec2_conn.delete_security_group (group_id = self.conf["sg_nat"]);
+        self.vpc_conn.disassociate_route_table(self.conf["custom_rtable_assoc"]);
+        self.vpc_conn.delete_route (self.conf["custom_rtable"], "0.0.0.0/0");
+        self.vpc_conn.delete_route_table (self.conf["custom_rtable"]);
+        self.vpc_conn.detach_internet_gateway (self.conf["ig"], self.conf["vpc"]);
+        self.vpc_conn.delete_internet_gateway (self.conf["ig"]);
+        self.vpc_conn.delete_subnet (self.conf["public_subnet"]);
+        self.vpc_conn.delete_vpc (self.conf["vpc"]);
+    
+    # Terminates NAT instance and returns when it get into 'terminated' state
+    def __terminate_nat_instance (self):
+        self.ec2_conn.terminate_instances (instance_ids = [self.conf["nat_instance"]]);
+    
+        while True:
+            reservation = self.ec2_conn.get_all_instances (instance_ids = 
+                                                        [self.conf["nat_instance"]]);
+            if reservation[0].instances[0].state != 'terminated':
+                print "instance {} shutting-down...".format(reservation[0].instances[0].id);
+                time.sleep(10);
+            else:
+                return;
+
+if __name__ == '__main__':
+    if len (sys.argv) != 2:
+        print 'usage: awsCoreRelease.py <conf_file>';    
+        sys.exit(-1);
+    
+    acr = awsCoreRelease(sys.argv[1]);
