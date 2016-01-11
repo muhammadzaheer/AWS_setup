4ff8b424c8228721dc6838e51af6bb2b4847a5c6
ashhadsheikh <ashhadsheikh@users.noreply.github.com>
2016-01-11 11:47:09 +0500

    Added comments

--- src/awsconn.py
+++ src/awsconn.py
@@ -10,12 +10,12 @@ class awsConn (object):
     
     @staticmethod
     def create_ec2_conn_singapore():
-        """ Creates an ec2 connection in Singapore Region """ 
+        """ Gets the list of all the available regions and creates an ec2 connection in Singapore Region """ 
         singapore = boto.ec2.regions()[4];
         return boto.ec2.connect_to_region (region_name=singapore.name);
 
     @staticmethod
     def create_vpc_conn_singapore():
-        """ Create a VPC connection in Singapore Region """
+        """  Gets the list of all the available regions and create a VPC connection in Singapore Region """
         singapore = boto.ec2.regions()[4];
         return boto.vpc.VPCConnection (region=singapore);
